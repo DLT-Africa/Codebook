12ef2ddb2c91a95d8a5f809fc3682c0d1f0169b0
DLT-Africa <142330919+DLT-Africa@users.noreply.github.com>
2025-05-15 10:49:12 +0100

    Learning about branching system

---
+++ src/components/ExampleComponent.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ExampleComponent = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias odio
+      delectus distinctio iusto illum numquam cupiditate quidem quam mollitia ea
+      architecto similique soluta sequi, commodi, aut beatae aliquid vero ex?
+    </div>
+  );
+};
+
+export default ExampleComponent;
